9b72617f83454f8a01c7fd20e7ada17a242e7621
mktsdv <marketing.midia.sdv@gmail.com>
2025-05-23 21:33:14 -0300

    a

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.querySelector("main.container");
   const sections = container.querySelectorAll("section");
+  const menuIcon = document.getElementById("menu-icon");
+  const menu = document.getElementById("menu");
+  const links = menu.querySelectorAll("a");
+
+  menuIcon.addEventListener("click", () => {
+    menu.classList.toggle("show");
+  });
+
+  links.forEach(link => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const targetId = link.getAttribute("href").replace("#", "");
+      const targetSection = document.querySelector(`.${targetId}, #${targetId}`);
+
+      if (targetSection) {
+        targetSection.scrollIntoView({ behavior: "smooth" });
+      }
+
+      // Fecha o menu após clique em mobile
+      if (window.innerWidth <= 600) {
+        menu.classList.remove("show");
+      }
+    });
+  });
 
   let currentIndex = 0;
   let isThrottled = false;
