b1df1c4584aa28f660fd1426701f9f95f82a1010
mktsdv <marketing.midia.sdv@gmail.com>
2025-05-26 15:12:27 -0300

    a

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
     link.addEventListener("click", (e) => {
       e.preventDefault();
       const targetId = link.getAttribute("href").replace("#", "");
-      const targetSection = document.querySelector(`.${targetId}, #${targetId}`);
+      const targetSection = document.querySelector(.${targetId}, #${targetId});
 
       if (targetSection) {
         targetSection.scrollIntoView({ behavior: "smooth" });
@@ -28,11 +28,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Ajusta a altura de cada section para 100vh
   const setSectionHeights = () => {
-  const usableHeight = window.innerHeight * 0.73; // 100vh - 17vh (header) - 10vh (footer)
-  sections.forEach(section => {
-    section.style.height = `${usableHeight}px`;
-  
-};
+    const vh = window.innerHeight;
+    sections.forEach(section => section.style.height = ${vh}px);
+  };
 
   window.addEventListener("resize", setSectionHeights);
   setSectionHeights();
